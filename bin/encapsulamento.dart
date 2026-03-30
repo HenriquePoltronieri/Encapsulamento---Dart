7f6ec571ce3c6de135387204e611601fc278cb41
Henrique Poltronieri <henrique.poltronieri2008@gmail.com>
2026-03-30 11:08:35 -0300

    Refactor main function and remove salvar method

--- bin/encapsulamento.dart
+++ bin/encapsulamento.dart
@@ -1,54 +1,18 @@
 import 'dart:io';
 import 'dart:convert';
-import 'package:encapsulamento/encapsulamento.dart';
-
-
-void salvar(Conta conta){
-  final arquivo = File('./dados.json');
-  arquivo.writeAsStringSync(jsonEncode(conta.toJson()));
-}
-
+import '../lib/conteudo.dart';
 
 void main() {
- Conta minhaConta;
 
- final arquivo = File('./dados.json');
+  String conteudoArquivo = File('dados.json').readAsStringSync();
 
- if(arquivo.existsSync()){
-  var dados = jsonDecode(arquivo.readAsStringSync());
-  minhaConta = ContaCorrente(dados['titular']);
-  minhaConta.atualizarSaldo = dados['saldo'];
+  Map<String, dynamic> dados = jsonDecode(conteudoArquivo);
 
- }else{
-  minhaConta = ContaCorrente('Igor');
- }
+  Filme filme = Filme(dados['titulo']);
 
- while (true) {
-   print(
-     '\nTitular: ${minhaConta.titular} | Saldo: R\$ ${minhaConta.saldoAtual}',
-   );
-   stdout.write('[1] Depósito | [2] Saque | [3] Sair: ');
-   var op = stdin.readLineSync();
-
-
-   if (op == '3') break;
-
-
-   try {
-     if (op == '1') {
-       stdout.write('Valor: ');
-       double v = double.parse(stdin.readLineSync()!);
-       minhaConta.depositar(v);
-     } else if (op == '2') {
-       stdout.write('Valor: ');
-       double v = double.parse(stdin.readLineSync()!);
-       minhaConta.sacar(v);
-     }
-
-
-    salvar(minhaConta);
-   } catch (e) {
-     print('⚠️ Erro: $e');
-   }
- }
+  try {
+    filme.validarClassificacao = dados['classificacao'];
+  } catch (e) {
+    print(e);
+  }
 }
